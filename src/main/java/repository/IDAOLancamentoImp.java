c3764ef8d906a02079ba54922fa53382a8fa0f00
Gustavo.sousa <gustavo.sousa@castgroup.com.br>
2023-08-05 21:28:26 -0300

    consulta a lancamentos por datas

--- src/main/java/repository/IDAOLancamentoImp.java
+++ src/main/java/repository/IDAOLancamentoImp.java
@@ -1,6 +1,7 @@
 package repository;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -37,6 +38,70 @@ public class IDAOLancamentoImp implements IDAOLancameto, Serializable {
 		retorno = entityManager.createQuery("FROM Lancamento where usuariologado_id = :login_user ORDER BY id desc", Lancamento.class)
 				.setParameter("login_user", idUser)
 				.setMaxResults(3)
+				.getResultList();
+		entityTransaction.commit();
+		entityManager.close();
+		return retorno;
+	}
+
+
+	@Override
+	public List<Lancamento> consultaLancamentosPorNumeroNota(String numeroNota) {
+		List<Lancamento> retorno = null;
+		EntityManager entityManager = JPAUTIL.getEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT lanc FROM Lancamento lanc");
+		sql.append(" WHERE lanc.numeroNotaFiscal='").append(numeroNota.trim()).append("'");
+		retorno = entityManager.createQuery(sql.toString())
+				.getResultList();
+		entityTransaction.commit();
+		entityManager.close();
+		return retorno;
+		
+	}
+
+
+	@Override
+	public List<Lancamento> consultaLancamentosPorDataInicial(Date dataInicial) {
+		List<Lancamento> retorno = null;
+		EntityManager entityManager = JPAUTIL.getEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		retorno = entityManager.createQuery("FROM Lancamento where dataInicial >= :data_inicial ORDER BY id desc", Lancamento.class)
+				.setParameter("data_inicial", dataInicial)
+				.getResultList();
+		entityTransaction.commit();
+		entityManager.close();
+		return retorno;
+	}
+
+
+	@Override
+	public List<Lancamento> consultaLancamentosPorDataFinal(Date dataFinal) {
+		List<Lancamento> retorno = null;
+		EntityManager entityManager = JPAUTIL.getEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		retorno = entityManager.createQuery("FROM Lancamento where dataFinal <= :data_final ORDER BY id desc", Lancamento.class)
+				.setParameter("data_final", dataFinal)
+				.getResultList();
+		entityTransaction.commit();
+		entityManager.close();
+		return retorno;
+	}
+
+
+	@Override
+	public List<Lancamento> consultaLancamentosPorDataInicialFinal(Date dataInical, Date dataFinal) {
+		List<Lancamento> retorno = null;
+		EntityManager entityManager = JPAUTIL.getEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		entityTransaction.begin();
+		retorno = entityManager.createQuery("FROM Lancamento where dataFinal <= :data_final and dataInicial >= :data_inicial ORDER BY id desc", Lancamento.class)
+				.setParameter("data_final", dataFinal)
+				.setParameter("data_inicial", dataInical)
 				.getResultList();
 		entityTransaction.commit();
 		entityManager.close();
--- src/main/java/repository/IDAOLancameto.java
+++ src/main/java/repository/IDAOLancameto.java
@@ -1,5 +1,6 @@
 package repository;
 
+import java.util.Date;
 import java.util.List;
 
 import entity.Lancamento;
@@ -9,5 +10,13 @@ public interface IDAOLancameto {
 	List<Lancamento> consultar (Long idUser);
 	
 	List<Lancamento> consultar3PorPagina(Long idUser);
+	
+	List<Lancamento> consultaLancamentosPorNumeroNota(String numeroNota);
+	
+	List<Lancamento> consultaLancamentosPorDataInicial(Date dataInicial);
+	
+	List<Lancamento> consultaLancamentosPorDataFinal(Date dataFinal);
+	
+	List<Lancamento> consultaLancamentosPorDataInicialFinal(Date dataInical,Date dataFinal);
 
 }
